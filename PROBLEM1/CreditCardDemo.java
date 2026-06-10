c9e54cbf398d9036659d739fd13420802c91ce31
Rebecca Miller <rebecca.miller@keyin.com>
2026-06-09 23:30:21 -0230

    fixed credit card demo

--- PROBLEM1/CreditCardDemo.java
+++ PROBLEM1/CreditCardDemo.java
@@ -4,12 +4,17 @@ public class CreditCardDemo {
 
 public static void main (String [] args) {
 
-    final Money LIMIT = new Money(1000); final Money FIRST_AMOUNT = new Money(200); final Money SECOND_AMOUNT = new Money(10.02); final Money THIRD_AMOUNT = new Money(25); final Money FOURTH_AMOUNT = new Money(990);
+    final Money LIMIT = new Money(1000); 
+    final Money FIRST_AMOUNT = new Money(200); 
+    final Money SECOND_AMOUNT = new Money(10.02); 
+    final Money THIRD_AMOUNT = new Money(25); 
+    final Money FOURTH_AMOUNT = new Money(990);
 
     Person owner = new Person(“Christie”, “Diane”, 
     new Address(“237J Harvey Hall”,”Menomonie”, 
     “WI” , “54751”)); 
-    CreditCard visa = new CreditCard(owner, LIMIT); 
+
+    CreditCard visa = new CreditCard(owner, LIMIT, new Money(0)); 
     System.out.println(visa.getPersonals()); 
     System.out.println(“Balance: ” + visa.getBalance()); 
     System.out.println(“Credit Limit : ” + visa.getCreditLimit()); 
--- PROBLEM1/Person.java
+++ PROBLEM1/Person.java
@@ -3,6 +3,12 @@ public class Person {
     private String firstName;
     private Address home;
 
+    public Person(String lastName, String firstName, Address home) {
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.home = home;
+    }
+
     public String toString() {
         return String.format("%s, %s\n%s", lastName, firstName, home);
     }
